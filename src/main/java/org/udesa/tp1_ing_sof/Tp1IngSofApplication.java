76b11a89d221df38d644c0f1c5ae5a3e24d1d4bc
Francisco Nattero <fnattero@udesa.edu.ar>
2025-09-12 19:38:12 -0300

    primer commit

---
+++ src/main/java/org/udesa/tp1_ing_sof/Tp1IngSofApplication.java
@@ -0,0 +1,13 @@
+package org.udesa.tp1_ing_sof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1IngSofApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1IngSofApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/udesa/tp1_ing_sof/Tp1IngSofApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udesa.tp1_ing_sof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1IngSofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
