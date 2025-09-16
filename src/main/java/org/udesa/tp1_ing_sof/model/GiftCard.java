69cadf5ea6c4d37e54649e22ab19b612463fb7d1
jeremias-paschmann_rappinc <jeremias.paschmann@rappi.com>
2025-09-16 18:31:14 -0300

    ping

--- src/main/java/org/udesa/tp1_ing_sof/model/GiftCard.java
+++ src/main/java/org/udesa/tp1_ing_sof/model/GiftCard.java
@@ -34,7 +34,7 @@ public class GiftCard {
     }
 
     public boolean isClaimed() {
-        return state.isClaimed();
+        return state.isClaimed();//ping
     }
 
     private static abstract class GiftCardState {
